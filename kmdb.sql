11a7d9ce20a88f42564b5b7ebfc3fb9372f0505c
Ignacio-Perez-2023 <ignacio.lopez@kellogg.northwestern.edu>
2023-01-24 17:56:07 +0000

    Advance on hw1 (creating tables)

--- kmdb.sql
+++ kmdb.sql
@@ -102,8 +102,35 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+ DROP TABLE IF EXISTS movies;
+ DROP TABLE IF EXISTS studios;
+ DROP TABLE IF EXISTS characters;
+ DROP TABLE IF EXISTS actors;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+   year_released TEXT,
+   MPAA_rating TEXT,
+   studio TEXT,
+   character_1 TEXT,
+   character_2 TEXT,
+   character_3 TEXT,
+   character_4 TEXT,
+   character_5 TEXT);
+
+CREATE TABLE studios (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id TEXT,
+   name TEXT,);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_id TEXT,
+   name TEXT,);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
